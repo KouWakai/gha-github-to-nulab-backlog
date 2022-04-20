f0fcdddbdbd467248b5a9723ecadc2849aa842f6
KouWakai <kouyama112@gmail.com>
2022-04-20 17:58:37 +0900

    build

--- run.js
+++ run.js
@@ -7,8 +7,9 @@ async function run() {
     const context = github.context;
     const payload = context.payload;
 
+    console.log(context);
     console.log(payload);
-    return;
+    
     // Retrieve GITHUB_TOKEN from environment variable
     // Do nothing when GITHUB_TOKEN does not exist
     const token = process.env['GITHUB_TOKEN'];
@@ -21,21 +22,22 @@ async function run() {
     // Create octokit clients
     const octokit = new github.getOctokit(token);
 
-
-
-    // GITHUB_REPOSITORY is GitHub Action's built-in environment variable
-    // https://help.github.com/en/articles/virtual-environments-for-github-actions#environment-variables
     const repoWithOwner = process.env['GITHUB_REPOSITORY'];
     const [owner, repo] = repoWithOwner.split('/');
-    console.log(repoWithOwner);
-    console.log(context.issue.number);
 
-    const response = await octokit.rest.issues.createComment({
+
+
+    const response = await octokit.rest.issues.listComments({
       owner,
       repo,
       issue_number: context.issue.number,
-      body: "1",
     });
+
+
+    console.log(response);
+    console.log(context.issue.number);
+
+    
   } catch (error) {
     core.setFailed(error.message);
   }
