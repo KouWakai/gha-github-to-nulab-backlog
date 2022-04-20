5c38b32345b881203621a0873bfddbebd7e1e757
KouWakai <kouyama112@gmail.com>
2022-04-20 17:40:05 +0900

    不要なコメントを消した

--- run.js
+++ run.js
@@ -20,8 +20,7 @@ async function run() {
       priorityId: `${priorityid}`,
       summary: `${title}`
     };
-    console.log(process.env.issuecomment)
-    console.log(process.env.domain)
+    
     // headerでコンテンツタイプを指定
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
