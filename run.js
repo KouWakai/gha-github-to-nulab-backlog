824616306bab4c58ac3656c2d2f515a8adbf29f5
KouWakai <kouyama112@gmail.com>
2022-04-20 11:26:12 +0900

    build2

--- run.js
+++ run.js
@@ -1,6 +1,5 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const axios = require('axios')
 
 async function run() {
   try {
@@ -16,12 +15,14 @@ async function run() {
     }
 
     // Create octokit client
-  const octokit = new github.GitHub(token);
+    const octokit = new github.GitHub(token);
 
     // GITHUB_REPOSITORY is GitHub Action's built-in environment variable
     // https://help.github.com/en/articles/virtual-environments-for-github-actions#environment-variables
     const repoWithOwner = process.env.GITHUB_REPOSITORY;
     const [owner, repo] = repoWithOwner.split('/');
+    console.log(repoWithOwner);
+    console.log(context.issue.number);
 
     const response = await octokit.issues.createComment({
       owner,
@@ -29,7 +30,6 @@ async function run() {
       issue_number: context.issue.number,
       body: 1,
     });
-
   } catch (error) {
     core.setFailed(error.message);
   }
