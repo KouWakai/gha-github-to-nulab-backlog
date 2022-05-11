2d9cdab19c2ca06772c0ccdebd31f9a4c91c7136
Kodai Wakai <35868597+KouWakai@users.noreply.github.com>
2022-05-11 18:19:57 +0900

    Update run.js

--- run.js
+++ run.js
@@ -7,8 +7,6 @@ async function run() {
   try {
     const type = github.context.payload.action;
     if (type == 'created') {
-      console.log("comment is posted on Github!!");
-      console.log("test");
       create_comment();
     }else if(type == 'opened'){
       console.log("issue is opend in Github!!")
